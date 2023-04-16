229d2ecceff0720b8bd0fb6159d4e508047bd47a
Bence Boros <borosbence6800@gmail.com>
2023-04-16 13:49:14 +0200

    Add project files.

---
+++ Vakcina.Terminal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Vakcina.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adatok beolvasása...");
+
+            // TODO: 01. adatok beolvasása és tárolása OOP formában
+
+            // TODO: 02. beolvasott elemek száma
+
+            // TODO: 03. hibásan felvitt adat keresése
+
+            // TODO: 04. Felhasználónév jelszó egyezés
+
+            // TODO: 05. Szűrés, rendezés, jelszó titkosítás
+
+            // TODO: 06. export készítése
+
+            Console.ReadKey();
+        }
+    }
+}
